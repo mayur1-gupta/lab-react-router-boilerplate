dd69a0d2114c207089636c802f904e3bebb84a82
mayur1-gupta <144005425+mayur1-gupta@users.noreply.github.com>
2024-02-29 10:58:38 +0530

    Update Heading.jsx

--- my_app/src/component/Heading.jsx
+++ my_app/src/component/Heading.jsx
@@ -5,8 +5,8 @@ function Heading() {
     <div className="navbar">
      <h3> <Link to="/"> Kalvium ❤️</Link> </h3>
      <ul>
-      <Link to="/contact">Contacts</Link>
-      <Link to="/about">About</Link>
+      <Link to="/CONTACTS">Contacts</Link>
+      <Link to="/ABOUT">About</Link>
     </ul>
     </div>
   )
